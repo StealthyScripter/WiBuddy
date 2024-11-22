40234b8b4e3ed84ae5713060fc1e3ce6fb0d4f63
StealthyScripter <wendotbrian@gmail.com>
2024-11-22 03:13:11 -0600

    Added dropdown to select project and technology during task entry

--- flask-backend/init_db.py
+++ flask-backend/init_db.py
@@ -1,11 +1,11 @@
 from app import app, db
 from app.models import *
-from tests.populate import populate_data
 
 
 def init_db():
-    db.create_all()
-    print("Database initialized successfully")
+    with app.app_context():
+        db.create_all()
+        print("Database initialized successfully")
 
 if __name__ == '__main__':
     init_db()
