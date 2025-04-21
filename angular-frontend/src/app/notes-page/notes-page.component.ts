bbbd0c23d5aeba73c2d15fab238fc47cf97e1775
StealthyScripter <wendotbrian@gmail.com>
2025-04-20 22:31:45 -0400

    fixed responsive layout on notes page

--- angular-frontend/src/app/notes-page/notes-page.component.ts
+++ angular-frontend/src/app/notes-page/notes-page.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Router} from '@angular/router';
 import { Note } from '../../models.interface';
 import { mockNotes } from '../../test-data/task.data';
 
@@ -11,12 +11,10 @@ import { mockNotes } from '../../test-data/task.data';
   templateUrl: './notes-page.component.html',
   styleUrl: './notes-page.component.css'
 })
-export class NotesPageComponent {
+export class NotesPageComponent{
     notes: Note[] = mockNotes;
 
-    openNote(noteId: string) {
-      // Implement navigation to note detail
-      console.log('Opening note:', noteId);
+    constructor(private router: Router){
     }
 
     getNotesPreview(notes: string[]): string {
@@ -32,5 +30,13 @@ export class NotesPageComponent {
       }
 
       return content;
+    }
+
+    navigateToNote(noteId: string){
+      this.router.navigate(['notes-details',noteId]);
+    }
+
+    navigateToNewNote() {
+      this.router.navigate(['add-notes']);
     }
   }
